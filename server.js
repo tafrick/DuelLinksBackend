3503c9433c1ac52f0ba9ded341b7a7085ea74173
Tyler Frick <tyfrick25@gmail.com>
2020-10-15 13:06:51 -0700

    undid change

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const cors = require('cors')
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const router = express.Router()
 
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
@@ -19,11 +18,6 @@ db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
 app.use(express.json())
-
-app.use(router)
-app.get("/", (req, res) => {
-    res.send("Hello from root")
-})
 
 const boxesRouter = require('./routes/boxes')
 app.use(cors())
